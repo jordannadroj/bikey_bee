aae6dfc8fbc4e10df7aa3e2815edbe6193d02b60
Igor Stein <igor_stein@yahoo.de>
2021-01-26 20:23:58 +0100

    do new.html.erb and change bootstrap primary and border-radius

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -8,8 +8,7 @@ class BikesController < ApplicationController
   end
 
   def create
-    current_user # how can i appoint the current_user by default?
-    raise
+    @bike.current_user
     @bike = Bike.new(bike_params)
     if @bike.save
       redirect_to bikes_path(@bike)
@@ -21,6 +20,6 @@ class BikesController < ApplicationController
   private
 
   def bike_params
-    params.require(:bike).permit(:name, :category, :size, :price, :location, :user_id)
+    params.require(:bike).permit(:name, :category, :size, :price, :location)
   end
 end
