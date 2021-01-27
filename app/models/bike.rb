a9d01f9897348c879a05a7acaaeffa3a76c9066a
Lena Blank <mine.blank@gmail.com>
2021-01-27 16:12:55 +0100

    add a map to show page

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -4,4 +4,7 @@ class Bike < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :name, :category, :size, :price, :location, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
