37ac799678b1771c8a8d5058fe4c145898d15658
Xavi Molina <xavimolina90@gmail.com>
2021-01-28 18:32:38 +0100

    commiting again to correct a typo, book a bike button should be functioning

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_073055) do
+ActiveRecord::Schema.define(version: 2021_01_27_122056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 2021_01_27_073055) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "picture"
+    t.text "description"
     t.float "latitude"
     t.float "longitude"
-    t.text "description"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
