3eb3ceb3639c4f81a53ee543014bbfddfb497430
Lena Blank <mine.blank@gmail.com>
2021-01-30 13:00:32 +0100

    customize a map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -8,22 +8,29 @@ const initMapbox = () => {
 
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/lenablank/ckkjnnz9o207u17nw2y586qem'
     });
 
-    const markers = JSON.parse(mapElement.dataset.markers);
-      markers.forEach((marker) => {
-        new mapboxgl.Marker()
-          .setLngLat([ marker.lng, marker.lat ])
-          .addTo(map);
-    });
-    const fitMapToMarkers = (map, markers) => {
-      const bounds = new mapboxgl.LngLatBounds();
-        markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-        map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+    const marker = JSON.parse(mapElement.dataset.marker);
+
+    const addMarkerToMap = (map, marker) => {
+
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
+      .addTo(map);
     };
 
-    fitMapToMarkers(map, markers);
+    const fitMapToMarker = (map, marker) => {
+      const bounds = new mapboxgl.LngLatBounds();
+      bounds.extend([ marker.lng, marker.lat ]);
+      map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+    };
+
+    addMarkerToMap(map, marker);
+    fitMapToMarker(map, marker);
   }
 };
 
