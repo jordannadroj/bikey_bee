6ad98cb77fdac79c77d2726ee2f02475f0e77edb
jordannadroj <jordansiemens93@gmail.com>
2021-01-30 14:06:49 +0100

    a lot

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
+
   end
+
+  def dashboard
+    # collect all booking wheere user id = current user id
+    # create route for this dash
+    @bookings = Booking.where(user_id: current_user)
+  end
+
+  def user_bikes
+    @bikes = Bike.where(user_id: current_user)
+  end
+
 end
